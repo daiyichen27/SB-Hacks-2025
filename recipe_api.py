115612c537fc682c9a3d76091a83694604d4a138
Anirudh Warrier <anirudh@warrier.net>
2025-01-11 17:40:34 -0800

    Some changes

--- recipe_api.py
+++ recipe_api.py
@@ -32,19 +32,39 @@ class RateLimitedRequester:
         
         response = requests.get(url, headers=headers, params=formatted_params)
 
+        # return response.json()
         return response.json()
 
 if __name__ == '__main__':
     new_request = RateLimitedRequester()
-    ingredients = []
-    with open('input.txt', 'r') as file:
-        for line in file:
-            # Remove trailing newline and split on comma
-            row = line.rstrip('\n').split(',') 
-            for word in row:
-                ingredients.extend(row)
-    print(ingredients)
-    params = {"ingredients": "apples,flour,sugar", "number": "2"}
+    # ingredients = []
+    # with open('input.txt', 'r') as file:
+    #     for line in file:
+    #         # Remove trailing newline and split on comma
+    #         row = line.rstrip('\n').split(',') 
+    #         for word in row:
+    #             ingredients.extend(row)
+    # print(ingredients)
+
+    # https://inputdata
+
+    input_url = "https://inputdata"
+    user_input = requests.get(input_url)
+
+    if user_input.status_code == 200:
+        print(user_input.text)
+    else:
+        print(f"Error: {user_input.status_code}")
+
+    params = {"ingredients": "cauliflower,onion,tomato", "number": "2"}
+
+    try:
+        data = new_request.retrieve(params)
+        with open("data.json", "w") as file:
+            json.dump(data, file)
+    except IOError as e:
+        print(f"An error occurred: {e}")
+    
     print(new_request.retrieve(params))
 
 
